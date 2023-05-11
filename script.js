2ebd5f6df95bb6d1ce94f7c968b9133a2671867b
Payalbh20 <117173906+Payalbh20@users.noreply.github.com>
2023-05-11 21:34:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,79 @@
-var itemName = document.getElementById("item-name-input");
-var itemQuantity = document.getElementById("item-qty-input");
-var itemPrice = document.getElementById("item-price-input");
- 
-var tbody = document.querySelector("tbody");
- 
-var grandTotal = document.getElementById("total");
-var grand_total = 0;
- 
-var addItem = document.querySelector("#add");
- 
-addItem.addEventListener("click", (event) => {
-  event.preventDefault();
- 
-  let newRow = document.createElement("tr");
- 
-  let name = document.createElement("td");
-  name.innerText = itemName.value;
-  name.classList.add("item");
-  newRow.append(name);
- 
-  let price = document.createElement("td");
-  price.innerText = itemPrice.value;
-  newRow.append(price);
-  price.classList.add("price");
- 
-  let quantity = document.createElement("td");
-  quantity.innerText = itemQuantity.value;
-  newRow.append(quantity);
- 
-  if (itemName.value && itemQuantity.value && itemPrice.value) {
-    let total = document.createElement("td");
-    total.innerText = itemPrice.value * itemQuantity.value;
-    newRow.append(total);
- 
-    tbody.append(newRow);
- 
-    grand_total += Number(total.innerText);
-    grandTotal.innerHTML = "Grand Total: ₹ " + grand_total;
+const products = [
+  { id: 1, name: "Product 1", price: 10 },
+  { id: 2, name: "Product 2", price: 20 },
+  { id: 3, name: "Product 3", price: 30 },
+  { id: 4, name: "Product 4", price: 40 },
+  { id: 5, name: "Product 5", price: 50 },
+];
+
+// DOM elements
+const productList = document.getElementById("product-list");
+const cartList = document.getElementById("cart-list");
+const clearCartBtn = document.getElementById("clear-cart-btn");
+
+// Get cart data from session storage or initialize it
+let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
+
+// Render product list
+function renderProducts() {
+  products.forEach((product) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
+    productList.appendChild(li);
+  });
+}
+
+// Render cart list
+function renderCart() {
+  cartList.innerHTML = "";
+  cart.forEach((cartItem) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${cartItem.name} - $${cartItem.price} <button class="remove-from-cart-btn" data-id="${cartItem.id}">Remove from Cart</button>`;
+    cartList.appendChild(li);
+  });
+}
+
+// Add item to cart
+function addToCart(productId) {
+  const productToAdd = products.find((product) => product.id === productId);
+  if (productToAdd) {
+    cart.push(productToAdd);
+    sessionStorage.setItem("cart", JSON.stringify(cart));
+    renderCart();
   }
- 
-  itemName.value = "";
-  itemQuantity.value = "";
-  itemPrice.value = "";
+}
+
+// Remove item from cart
+function removeFromCart(productId) {
+  cart = cart.filter((cartItem) => cartItem.id !== productId);
+  sessionStorage.setItem("cart", JSON.stringify(cart));
+  renderCart();
+}
+
+// Clear cart
+function clearCart() {
+  cart = [];
+  sessionStorage.removeItem("cart");
+  renderCart();
+}
+
+// Event listeners
+productList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("add-to-cart-btn")) {
+    const productId = parseInt(event.target.getAttribute("data-id"));
+    addToCart(productId);
+  }
 });
+
+cartList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("remove-from-cart-btn")) {
+    const productId = parseInt(event.target.getAttribute("data-id"));
+    removeFromCart(productId);
+  }
+});
+
+clearCartBtn.addEventListener("click", clearCart);
+
+// Initial render
+renderProducts();
+renderCart();
